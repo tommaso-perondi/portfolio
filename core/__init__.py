7ce27dc3fd5b21dea0501d9af7265b8f0e3b57e5
Tommaso <tommaso2001.pero@gmail.com>
2018-04-30 11:58:14 +0200

    Fixing paths

--- core/__init__.py
+++ core/__init__.py
@@ -2,14 +2,14 @@ import os
 
 from flask import Flask
 from flask_mail import Mail
-from flask.ext.cache import Cache
-
+from flask_cache import Cache
+project_path = os.path.dirname(os.path.realpath(__file__))
 app = Flask(__name__, instance_relative_config=False)
 cache = Cache(app, config={'CACHE_TYPE': 'simple'})
 
 app.config.from_pyfile('config.py', silent=True)
 app.config.from_pyfile('credentials.txt', silent=True)
 mail = Mail(app)
-mail_html = open("core/templates/mail.html", "r").read()
+mail_html = open(project_path + "core/templates/mail.html", "r").read()
 
 from core import routes

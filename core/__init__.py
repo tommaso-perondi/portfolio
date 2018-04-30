d495990d5370dee2dc7df3d790fc8d2f35c14dc7
Tommaso <tommaso2001.pero@gmail.com>
2018-04-30 11:59:24 +0200

    Fixing paths

--- core/__init__.py
+++ core/__init__.py
@@ -10,6 +10,6 @@ cache = Cache(app, config={'CACHE_TYPE': 'simple'})
 app.config.from_pyfile('config.py', silent=True)
 app.config.from_pyfile('credentials.txt', silent=True)
 mail = Mail(app)
-mail_html = open(project_path + "core/templates/mail.html", "r").read()
+mail_html = open(project_path + "/templates/mail.html", "r").read()
 
 from core import routes
